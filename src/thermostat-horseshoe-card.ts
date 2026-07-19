977fda614a2392379bd0d6b97fc67a2e8e5f7089
oyunmfc-ops <oyunmfc@gmail.com>
2026-07-19 22:36:24 +0300

    Update thermostat-horseshoe-card.ts

--- src/thermostat-horseshoe-card.ts
+++ src/thermostat-horseshoe-card.ts
@@ -138,44 +138,4 @@ export class ThermostatHorseshoeCard extends LitElement {
     `;
   }
 
-  private polar(
-    cx:number,
-    cy:number,
-    r:number,
-    angle:number
-  ){
-
-    const rad=(angle-90)*Math.PI/180;
-
-    return{
-
-      x:cx+r*Math.cos(rad),
-
-      y:cy+r*Math.sin(rad)
-
-    };
-
-  }
-
-  private describeArc(
-    cx:number,
-    cy:number,
-    r:number,
-    start:number,
-    end:number
-  ){
-
-    const startPoint=this.polar(cx,cy,r,end);
-
-    const endPoint=this.polar(cx,cy,r,start);
-
-    const largeArc=(end-start)<=180?0:1;
-
-    return `
-      M ${startPoint.x} ${startPoint.y}
-      A ${r} ${r} 0 ${largeArc} 0 ${endPoint.x} ${endPoint.y}
-    `;
-
-  }
-
 }
