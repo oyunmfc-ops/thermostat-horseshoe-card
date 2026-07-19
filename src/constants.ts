382a2f3e1ffa5bdb7b6a09877af80ff2d6989259
oyunmfc-ops <oyunmfc@gmail.com>
2026-07-19 23:00:43 +0300

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,26 @@
+export const CARD = {
+
+  CENTER_X: 250,
+  CENTER_Y: 250,
+
+  RADIUS: 170,
+
+  START_ANGLE: 225,
+
+  END_ANGLE: 495,
+
+  MIN_TEMP: 18,
+
+  MAX_TEMP: 30
+
+};
+
+export const COLORS = {
+
+  BACKGROUND: "#e0e0e0",
+
+  ACTIVE: "#ffd362",
+
+  TEXT: "#303030"
+
+};
